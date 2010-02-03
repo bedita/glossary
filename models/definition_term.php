29830a8ee20a6c1547d87fd69b7312209787faca
batopa <batopa@gmail.com>
2010-02-03 16:18:21 +0000

    addons, glossary, classrooms

---
+++ models/definition_term.php
@@ -0,0 +1,37 @@
+<?php
+/*-----8<--------------------------------------------------------------------
+ * 
+ * BEdita - a semantic content management framework
+ * 
+ * Copyright 2008 ChannelWeb Srl, Chialab Srl
+ * 
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * BEdita is distributed WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Affero GNU General Public License for more details.
+ * You should have received a copy of the Affero GNU General Public License 
+ * version 3 along with BEdita (see LICENSE.AGPL).
+ * If not, see <http://gnu.org/licenses/agpl-3.0.html>.
+ * 
+ *------------------------------------------------------------------->8-----
+ */
+
+/**
+ * Glossary definition_term object
+ *
+ * @version			$Revision: 2534 $
+ * @modifiedby 		$LastChangedBy: bato $
+ * @lastmodified	$LastChangedDate: 2009-12-29 13:23:14 +0100 (mar, 29 dic 2009) $
+ * 
+ * $Id: definition_term.php 2534 2009-12-29 12:23:14Z bato $
+ */
+
+class DefinitionTerm extends BeditaObjectModel {
+			
+	public $objectTypesGroups = array("leafs", "related");
+
+}
+?>
